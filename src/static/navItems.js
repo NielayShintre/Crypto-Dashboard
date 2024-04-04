5be7ffc2f400f2a1a3cc377083f8d0d787d500ba
Nielay_Shintre <nielayshintre18@gmail.com>
2024-04-04 14:36:29 +0530

    Add files via upload

---
+++ src/static/navItems.js
@@ -0,0 +1,35 @@
+// Export an array of objects, each containing a title and the name of the icon component
+export const navItems = [
+  {
+    title: "Assets",
+    iconName: "AiOutlinePieChart",
+  },
+  {
+    title: "Trade",
+    iconName: "BiTrendingUp",
+  },
+  {
+    title: "Pay",
+    iconName: "RiCoinsLine",
+  },
+  {
+    title: "For You",
+    iconName: "MdWeb",
+  },
+  {
+    title: "Learn and earn",
+    iconName: "AiOutlinePlusCircle",
+  },
+  {
+    title: "Notifications",
+    iconName: "RiNotification3Line",
+  },
+  {
+    title: "Invite Friends",
+    iconName: "BsPersonPlus",
+  },
+  // {
+  //   title: "Send a gift",
+  //   iconName: "AiOutlineGift",
+  // },
+];
